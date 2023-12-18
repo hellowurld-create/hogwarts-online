5c749edab1e823f1544c1670a6a1dea8e7c2aa96
hellowurld-create <madebyaugus@gmail.com>
2023-12-18 01:12:07 +0100

    This is my first commit

---
+++ src/main/java/code/grind/giftedschoolonline/GiftedSchoolOnlineApplication.java
@@ -0,0 +1,13 @@
+package code.grind.giftedschoolonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftedSchoolOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftedSchoolOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/grind/giftedschoolonline/GiftedSchoolOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package code.grind.giftedschoolonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftedSchoolOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
